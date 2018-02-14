08ec7030aec0f8b8a8f51a0b737e39f9d04d1025
rishabhjaishwal <35591206+rishabhjaishwal@users.noreply.github.com>
2018-02-14 15:39:28 +0530

    Update meta.php

--- meta.php
+++ meta.php
@@ -2,10 +2,10 @@
 <html lang="en-US">
 <head>
   <meta charset="UTF-8">
-  <meta name="description" content="Project and Programming code website">
-  <meta name="keywords" content="HTML,CSS,XML,JavaScript,Project,Java,Php">
-  <meta name="author" content="Rishabh">
+  <meta name="description" content="">
+  <meta name="keywords" content="">
+  <meta name="author" content="">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="icon" href="images/logo.png" type="image/x-icon">
+  <link rel="icon" href="" type="image/x-icon">
 
 
