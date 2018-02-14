c1f4228d17745cfc9ffa003b9a9f9a0e094f7706
rishabhjaishwal <35591206+rishabhjaishwal@users.noreply.github.com>
2018-02-14 15:38:27 +0530

    Add files via upload

---
+++ lib.php
@@ -0,0 +1,19 @@
+<link rel="stylesheet" href="Styles/css/w3.css" />
+<link rel="stylesheet" href="Styles/css/bootstrap.min.css" />
+<link rel="stylesheet" href="Styles/css/checkbox.css" />
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Oswald">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open Sans">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+<![endif]-->
+<title>Protute</title>
+<style>
+h1,h2,h3,h4,h5,h6 {font-family: "Oswald"}
+body {font-family: "Open Sans"}
+a:hover{text-decoration:none;}
+.w3-bar-item:hover{background:#993}
+mysidebar a:hover{text-decoration:none}
+
+</style>
+</head>
---
+++ meta.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+  <meta charset="UTF-8">
+  <meta name="description" content="Project and Programming code website">
+  <meta name="keywords" content="HTML,CSS,XML,JavaScript,Project,Java,Php">
+  <meta name="author" content="Rishabh">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="icon" href="images/logo.png" type="image/x-icon">
+
+
