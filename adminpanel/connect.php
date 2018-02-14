fa4b08d46c202bd2a2a8389128a863273a19ec78
rishabhjaishwal <35591206+rishabhjaishwal@users.noreply.github.com>
2018-02-14 15:52:27 +0530

    Update connect.php

--- adminpanel/connect.php
+++ adminpanel/connect.php
@@ -1,4 +1,8 @@
 <?php
-  $con=mysqli_connect("localhost","root","root","finalproject") or die("could not connect");
+  $con=mysqli_connect("localhost","Username","password","database_name") or die("could not connect");
   
 ?>
+<!--use your database name-->
+<!--use your Password-->
+<!--use your Username-->
+<!--use your server ip or localhost-->
